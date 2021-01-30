6be5ad321c500510b9b4b58a059ae9a907981b31
Eric M. Furst <furst@udel.edu>
2021-01-29 22:31:10 -0500

    shell script wrapper

--- unm.sh
+++ unm.sh
@@ -4,19 +4,16 @@
 # E. M. F. 2021
 #
 if [[ $# -ne 1 ]]; then
-    echo 'usage: uninmize filename.25'
+    echo 'usage: unm filename.25'
     exit 1
 fi
 
 if [[ -f "$1" ]]; then
     echo "Reading "$1"..."
     name=${1%.25}
-    cp $1 fort.25
-    art11 > $name".tmp"
-    asa $name".tmp" > $name".txt"
-    echo "Art11 output generated."
+    art1 < $1 | asa > $name".txt"
+    echo "Art11 output created."
     python3 ibm1403fort.py $name".txt"
-    mv $name".tmp" $name".txt"
 else
     echo "$1 does not exist."
 fi
